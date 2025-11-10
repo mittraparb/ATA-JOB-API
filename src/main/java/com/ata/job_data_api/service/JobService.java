f034d84ff91b1e31e3474ef336ec0e086597b9a7
Mittraparb Chanasakulniyom <mittraparb.chana@gmail.com>
2025-11-10 14:20:09 +0700

    Add Job Title Filter

--- src/main/java/com/ata/job_data_api/service/JobService.java
+++ src/main/java/com/ata/job_data_api/service/JobService.java
@@ -4,7 +4,10 @@ import com.ata.job_data_api.model.RawJobData;
 import com.ata.job_data_api.repository.JobRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
 import reactor.core.publisher.Flux;
+
+import java.util.function.Predicate;
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +16,25 @@ public class JobService {
 
     public Flux<RawJobData> getRawJobData() {
         return repository.findAllRaw();
+    }
+
+    public Flux<RawJobData> findJobs(MultiValueMap<String, String> queryParams) {
+        var rawJobs = repository.findAllRaw();
+        var combinedPredicate = buildFilterPredicate(queryParams);
+        rawJobs = rawJobs.filter(combinedPredicate);
+        return rawJobs;
+    }
+
+    private Predicate<RawJobData> buildFilterPredicate(MultiValueMap<String, String> params) {
+        var jobTitlePredicate = createJobTitlePredicate(params);
+        return jobTitlePredicate;
+    }
+
+    private Predicate<RawJobData> createJobTitlePredicate(MultiValueMap<String, String> params) {
+        var jobTitle = params.getFirst("job_title");
+        if (jobTitle != null) {
+            return job -> job.getJobTitle() != null && job.getJobTitle().equalsIgnoreCase(jobTitle.trim());
+        }
+        return job -> true;
     }
 }
--- src/main/java/com/ata/job_data_api/web/JobController.java
+++ src/main/java/com/ata/job_data_api/web/JobController.java
@@ -3,20 +3,31 @@ package com.ata.job_data_api.web;
 import com.ata.job_data_api.model.RawJobData;
 import com.ata.job_data_api.service.JobService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/job_data")
+@Slf4j
 public class JobController {
 
     private final JobService jobService;
 
+    //for testing
     @GetMapping("/all-raw")
     public Flux<RawJobData> getAllRawJobs() {
         return jobService.getRawJobData();
+    }
+
+    @GetMapping
+    public Flux<RawJobData> getFilteredJobs(@RequestParam MultiValueMap<String, String> queryParams) {
+        log.info("params ", queryParams);
+        return jobService.findJobs(queryParams);
     }
 }
